7f01ed0f85ad6adcb7a64e0ff3362908f4c00e3b
Jasper A. Visser <jasper.a.visser@gmail.com>
2013-02-26 08:30:50 +0100

    Prepare getting (un)marshaller

--- src/eu/portavita/xml/jaxb/Main.java
+++ src/eu/portavita/xml/jaxb/Main.java
@@ -18,9 +18,6 @@ public class Main {
 		 * Marshal a clinical document.
 		 * (extract marshal method)
 		 * (re-use marshaller)
-		 *
-		 * marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", new SampleNamespacePrefixMapper());
-		 * marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 		 */
 
 
@@ -33,4 +30,47 @@ public class Main {
 //		}
 //		System.out.println((new Date().getTime() - started.getTime()) + "ms");
 	}
+
+
+//	/**
+//	 * Marshaller of documents.
+//	 */
+//	private static Marshaller marshaller = null;
+//
+//	/**
+//	 * Returns the marshaller of documents.
+//	 *
+//	 * @return
+//	 *
+//	 * @throws JAXBException
+//	 */
+//	private static Marshaller getMarshaller() throws JAXBException {
+//		if (marshaller == null) {
+//			JAXBContext context = JAXBContext.newInstance(Document.class);
+//			marshaller = context.createMarshaller();
+//			marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", new SampleNamespacePrefixMapper());
+//			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+//		}
+//		return marshaller;
+//	}
+//
+//	/**
+//	 * Unmarshaller of documents.
+//	 */
+//	private static Unmarshaller unmarshaller = null;
+//
+//	/**
+//	 * Returns the unmarshaller of documents.
+//	 *
+//	 * @return
+//	 *
+//	 * @throws JAXBException
+//	 */
+//	private static Unmarshaller getUnmarshaller() throws JAXBException {
+//		if (unmarshaller == null) {
+//			JAXBContext context = JAXBContext.newInstance(Document.class);
+//			unmarshaller = context.createUnmarshaller();
+//		}
+//		return unmarshaller;
+//	}
 }
